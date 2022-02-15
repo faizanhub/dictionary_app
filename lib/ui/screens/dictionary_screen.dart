aff09d64c61448ba55a73f65e3b173af32a3efa5
Muhammad Faizan <faizii.jay@gmail.com>
2022-02-15 17:31:23 +0500

    initial commit

---
+++ lib/ui/screens/dictionary_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DictionaryScreen extends StatefulWidget {
+  const DictionaryScreen({Key? key}) : super(key: key);
+
+  @override
+  _DictionaryScreenState createState() => _DictionaryScreenState();
+}
+
+class _DictionaryScreenState extends State<DictionaryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
